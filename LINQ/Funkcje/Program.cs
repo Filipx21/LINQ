21d3a0af9e59cc08cee71be5366a4c8c96a9e845
Filipx21 <filip.zajac2277@gmail.com>
2021-10-30 20:02:24 +0200

    IEnumerator

---
+++ LINQ/Funkcje/Program.cs
@@ -0,0 +1,39 @@
+﻿using Funkcje.Models;
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> programmers = new Employee[]
+            {
+                new Employee { EmployeeId = 1, FirstName = "Marcin", LastName = "Nowak"},
+                new Employee { EmployeeId = 2, FirstName = "Tomek", LastName = "Kowal"},
+                new Employee { EmployeeId = 3, FirstName = "Jacek", LastName = "Sobota"},
+                new Employee { EmployeeId = 4, FirstName = "Adam", LastName = "Wrona"}
+            };
+
+            IEnumerable<Employee> drivers = new List<Employee>()
+            {
+                new Employee { EmployeeId = 5, FirstName = "Olek", LastName = "Sroka"},
+                new Employee { EmployeeId = 6, FirstName = "Pawel", LastName = "Wrobel"},
+                new Employee { EmployeeId = 7, FirstName = "Marek", LastName = "Piatek"}
+            };
+
+            IEnumerator<Employee> enumerator = drivers.GetEnumerator();
+
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
