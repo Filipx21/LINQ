2b2cb2d96c4e6464300b4c11e749d644338b987d
Filipx21 <filip.zajac2277@gmail.com>
2021-11-02 20:41:26 +0100

    Filtr

--- LINQ/Zapytania/Program.cs
+++ LINQ/Zapytania/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zapytania.Infrastructure;
 using Zapytania.Models;
 
 namespace Zapytania
