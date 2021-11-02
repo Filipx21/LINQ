74cb3326cabb3aa7e717e1dea9d25c385ae5de58
Filipx21 <filip.zajac2277@gmail.com>
2021-11-02 20:22:51 +0100

    New Ep

---
+++ LINQ/Zapytania/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zapytania.Models;
+
+namespace Zapytania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "Siedem", Genre = "Thriller", Rating = 8.3f, Year = 1995},
+                new Movie { Title = "Efekt motyla", Genre = "Thriller", Rating = 7.8f, Year = 2004},
+                new Movie { Title = "127 godzin", Genre = "Dramat", Rating = 7.1f, Year = 2010},
+                new Movie { Title = "Skazani na Shawshank", Genre = "Dramat", Rating = 8.7f, Year = 1994},
+                new Movie { Title = "Zielona mila", Genre = "Dramat", Rating = 8.6f, Year = 1999},
+                new Movie { Title = "Forrest Gump", Genre = "Komedia", Rating = 8.5f, Year = 1994},
+                new Movie { Title = "Piękny umysł ", Genre = "Dramat", Rating = 8.3f, Year = 2001},
+                new Movie { Title = "Gladiator", Genre = "Dramat", Rating = 8.1f, Year = 2000}
+            };
+
+        }
+    }
+}
