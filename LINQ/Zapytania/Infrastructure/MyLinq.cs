a14a24e283f3e3b0dc69805af822d3e3cbab52a8
Filipx21 <filip.zajac2277@gmail.com>
2021-11-02 20:40:19 +0100

    MyLinq

---
+++ LINQ/Zapytania/Infrastructure/MyLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zapytania.Infrastructure
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<T> Filtr<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+
+            foreach(var item in data)
+            {
+                if(predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
