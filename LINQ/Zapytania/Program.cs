6552970b78d6c43a8d660bbc947e05b3d5365ec3
Filipx21 <filip.zajac2277@gmail.com>
2021-11-03 19:13:26 +0100

    Odroczone wykonanie

--- LINQ/Zapytania/Program.cs
+++ LINQ/Zapytania/Program.cs
@@ -8,6 +8,46 @@ namespace Zapytania
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    var movies = new List<Movie>
+        //    {
+        //        new Movie { Title = "Siedem", Genre = "Thriller", Rating = 8.3f, Year = 1995},
+        //        new Movie { Title = "Efekt motyla", Genre = "Thriller", Rating = 7.8f, Year = 2004},
+        //        new Movie { Title = "127 godzin", Genre = "Dramat", Rating = 7.1f, Year = 2010},
+        //        new Movie { Title = "Skazani na Shawshank", Genre = "Dramat", Rating = 8.7f, Year = 1994},
+        //        new Movie { Title = "Zielona mila", Genre = "Dramat", Rating = 8.6f, Year = 1999},
+        //        new Movie { Title = "Forrest Gump", Genre = "Komedia", Rating = 8.5f, Year = 1994},
+        //        new Movie { Title = "Piękny umysł ", Genre = "Dramat", Rating = 8.3f, Year = 2001},
+        //        new Movie { Title = "Gladiator", Genre = "Dramat", Rating = 8.1f, Year = 2000}
+        //    };
+
+        //    //var query = movies.Filtr(x => x.Year > 2002).Take(1);
+
+        //    //var query = movies.Where(x => x.Year > 2002);
+
+        //    //foreach (var result in query)
+        //    //{
+        //    //    Console.WriteLine(result.Title);
+        //    //}
+
+        //    var query = movies.Filtr(x => x.Year > 2002).ToList();
+
+        //    Console.WriteLine("#################################");
+        //    int count = query.Count();
+        //    Console.WriteLine(count);
+
+
+        //    //var enumerator = query.GetEnumerator();
+        //    //while (enumerator.MoveNext())
+        //    //{
+        //    //    Console.WriteLine(enumerator.Current.Title);
+        //    //}
+
+        //    Console.ReadKey();
+
+        //}
+
         static void Main(string[] args)
         {
             var movies = new List<Movie>
@@ -22,30 +62,19 @@ namespace Zapytania
                 new Movie { Title = "Gladiator", Genre = "Dramat", Rating = 8.1f, Year = 2000}
             };
 
-            //var query = movies.Filtr(x => x.Year > 2002).Take(1);
+            var query = movies
+                .Filtr(x => x.Year > 2002)
+                .OrderByDescending(x => x.Rating);
+                //.ToList();
 
-            //var query = movies.Where(x => x.Year > 2002);
-
-            //foreach (var result in query)
-            //{
-            //    Console.WriteLine(result.Title);
-            //}
-
-            var query = movies.Filtr(x => x.Year > 2002);
-
-            Console.WriteLine("#################################");
-            int count = query.Count();
-            Console.WriteLine(count);
-
-
-            //var enumerator = query.GetEnumerator();
-            //while (enumerator.MoveNext())
-            //{
-            //    Console.WriteLine(enumerator.Current.Title);
-            //}
+            var enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Title);
+            }
 
             Console.ReadKey();
-
         }
+
     }
 }
