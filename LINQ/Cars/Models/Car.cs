22d9474d7c29a177426e6db33b1b7910feff474b
Filipx21 <filip.zajac2277@gmail.com>
2021-11-03 19:39:14 +0100

    Project

---
+++ LINQ/Cars/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public double Capacity { get; set; }
+        public int NumberOfCylinders { get; set; }
+
+        public int BurningCity { get; set; }
+        public int BurningHighway { get; set; }
+        public int BurningMixed{ get; set; }
+
+    }
+}
