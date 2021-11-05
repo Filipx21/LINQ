f87e5c639c6064b2c84c980dfe908d4299199d8c
Filipx21 <filip.zajac2277@gmail.com>
2021-11-06 00:03:51 +0100

    TakO

--- LINQ/Cars/Models/Car.cs
+++ LINQ/Cars/Models/Car.cs
@@ -13,7 +13,6 @@ namespace Cars.Models
         public string Model { get; set; }
         public double Capacity { get; set; }
         public int NumberOfCylinders { get; set; }
-
         public int BurningCity { get; set; }
         public int BurningHighway { get; set; }
         public int BurningMixed{ get; set; }
---
+++ LINQ/TakO/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TakO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new GenerateValue();
+            var inputs = input.InputData();
+            
+            Console.WriteLine("Posortowana lista");
+            foreach (var i in inputs.OrderByDescending(x => x.Weight))
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.WriteLine($"Najwiekszy element: {inputs.OrderByDescending(x => x.Weight).First()}");
+            Console.ReadKey();
+        }
+    }
+
+    public interface IInput
+    {
+        List<Input> InputData();
+    }
+
+    public class GenerateValue : IInput
+    {
+        private const int LOOP = 5;
+
+        public List<Input> InputData()
+        {
+            List<Input> data = new List<Input>();
+
+            for (var x = 0; x < LOOP; x++)
+            {
+                try
+                {
+                    Console.Write("Podaj produkt z wagą: ");
+                    var consoleInput = Console.ReadLine();
+                    var input = FetchInput(consoleInput);
+                    data.Add(input);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    --x;
+                }
+                Thread.Sleep(156);
+            }
+
+            return data;
+        }
+
+        private Input FetchInput(string consoleInput)
+        {
+            var elements = consoleInput.Split(' ');
+
+            var unit = CheckUnit(elements[1]);
+
+            return new Input()
+            {
+                Weight = CheckWeight(elements[0], unit),
+                Unit = "kg",
+                Name = CheckName(elements[2])
+            };
+        }
+
+        private string CheckName(string consoleName)
+        {
+            return consoleName;
+        }
+
+        private string CheckUnit(string consoleUnit)
+        {
+            string[] units = new string[] { "kg", "dag", "g", "mg" };
+
+            foreach(var unit in units)
+            {
+                if (unit.Equals(consoleUnit.ToLower()))
+                    return consoleUnit.ToLower();
+            }
+            throw new ArgumentException("Error: Invalid unit");
+        }
+
+        private double CheckWeight(string consoleWeight, string unit)
+        {
+            var regex = new Regex(@"\d");
+
+            if (!regex.IsMatch(consoleWeight))
+                throw new ArgumentException("Error: Invalid weight");
+
+            var weight = Convert.ToDouble(consoleWeight);
+
+            if (unit.Equals("kg"))
+                return weight;
+            else if (unit.Equals("dag"))
+                return weight * 0.01;
+            else if (unit.Equals("g"))
+                return weight * 0.001;
+            else if (unit.Equals("mg"))
+                return weight * 0.000001;
+
+            throw new ArgumentException("Error: Invalid weight");
+        }
+    }
+
+    public class Input
+    {
+        public double Weight { get; set; }
+        public string Unit { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} - {2}", Weight, Unit, Name);
+        }
+    }
+}
